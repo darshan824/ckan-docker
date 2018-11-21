980255e198f41d98f4bce394c67fecd24c3b5a67
Ubuntu <ubuntu@ip-172-30-0-218.ap-south-1.compute.internal>
2018-11-21 04:54:02 +0000

    made changes in db

--- postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
+++ postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
@@ -2,7 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '$DS_RO_PASS';
-    CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';
-    GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;
+    CREATE USER ckan_default with password 'admin123';
+    CREATE USER datastore_default with password 'admin123';
+    CREATE DATABASE ckan_default OWNER ckan_default ENCODING 'utf-8';
+    GRANT ALL PRIVILEGES ON DATABASE ckan_default TO ckan_default;
+    CREATE DATABASE datastore_default OWNER ckan_default ENCODING 'utf-8';
+    GRANT ALL PRIVILEGES ON DATABASE datastore_default to ckan_default; 
+    GRANT ALL PRIVILEGES ON DATABASE datastore_default to datastore_default;
 EOSQL
